3f9d225879fd66194e716bb3ce2be2d0752f6eb0
Xeiniya <xeiniya@gmail.com>
2024-10-23 10:29:01 +0300

    Update widget.h

--- widget.h
+++ widget.h
@@ -2,41 +2,46 @@
 #define WIDGET_H
 
 #include <QWidget>
-#include <QDir>
 #include <QFileSystemModel>
-#include <QModelIndex>
-#include <QGridLayout>
-#include <QListView>
 #include <QImageWriter>
 #include <QTableWidget>
 #include <QFileDialog>
+#include <QGridLayout>
 #include <QHeaderView>
+#include <QImage>
+#include <QListView>
+#include <QPalette>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class Widget; }
-QT_END_NAMESPACE
+namespace Ui {
+class Widget;
+}
 
 class Widget : public QWidget
 {
     Q_OBJECT
 
 public:
-    Widget(QWidget *parent = nullptr);
+    explicit Widget(QWidget *parent = nullptr);
     ~Widget();
+
+private slots:
+    void on_listView_doubleClicked(const QModelIndex &index);
+    void dialogClose();
+    void on_multiChoice_clicked();
+    void on_selectFolderButton_clicked();
 
 private:
     Ui::Widget *ui;
-    bool line_changed=false;
     QFileSystemModel *model;
-    QGridLayout *backgr;
-    QListView *listView;
-    QTableWidget *twInfo=nullptr;
-    int *sortcolumns = new int[6];
-public slots:
-    void on_listView_doubleClicked(const QModelIndex &index);
-    void twInfoSelected(int logicalIndex);
-private slots:
-    void dialogClose();
-    void on_multiChoice_clicked();
+    QTableWidget *twInfo;
+    int sortcolumns[6];
+    std::function<bool(QTableWidgetItem*, QTableWidgetItem*)> functions[6];
+
+    void fillSortColumns();
+    void displayImageInfo(const QModelIndex &index, const QFileInfo &fileInfo);
+    void setTableHeaders();
+    void populateTableRow(const QString &filePath, int rowIndex);
+    bool isValidImageExtension(const QString &extension);
 };
+
 #endif // WIDGET_H
